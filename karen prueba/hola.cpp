1836c40ec0ff4e9f445d87fe72b48ff5bce36ed2
Karen Denegri <31105002+Karendc1d@users.noreply.github.com>
2017-08-28 22:33:08 -0500

    Update hola.cpp

--- karen prueba/hola.cpp
+++ karen prueba/hola.cpp
@@ -7,3 +7,20 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+
+
+#include 
+int main(void)
+{
+for (int j=2;j<=100;j++){//por definicion el 1 no es primo, se empieza en 2
+int a=0;
+for(int i=1;i<=100;i++)//divide a j entre los números del 1 al 100
+{
+    if(j%i==0) // si num1 módulo de i es 0, incrementamos a en 1.
+    a++;
+}
+if (a==2){ 
+printf("%d\n", j);
+}
+ }
+}
