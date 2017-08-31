01afb8410eb2b14ad4238eba8797449a6bead90c
Karen Denegri <31105002+Karendc1d@users.noreply.github.com>
2017-08-31 11:26:57 -0500

    Update hola.cpp

--- karen prueba/hola.cpp
+++ karen prueba/hola.cpp
@@ -33,3 +33,66 @@ int main()
     nc=0;
   }
 }
+
+
+
+
+
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num,n,p,i;
+  cout<<"Ingresa posicion";
+  cin>>num;
+  n=2;
+  i=0;
+  while (i<=num) 
+  { for (p=2;n%p!=0;p=p+1);
+  if(p==n)
+  {i=i+1;
+   if(i==num)
+   cout<<n<<endl;
+}
+n=n+1;
+}
+return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int n,mayor,menor;
+double b;
+double acumula=0.0;
+cout <<"Cuantos numeros quiere ingresar: " << endl;
+cin >> n;
+for (int cont=0,valor; n>cont; cont =cont+1){
+  cout <<"Ingrese un numero: "<< endl;
+  cin>>valor;
+  if(cont==1){
+    mayor=valor;
+    menor= valor;
+  }
+  else{
+    mayor=valor>mayor?valor:mayor;
+    menor=valor<menor?valor:menor;
+}
+  }
+   cout<<"El mayor es"<<mayor<<endl;
+    cout<<"El menor es"<<menor<<endl;
+    b=mayor+menor//2;
+;cout<<"El promedio es"<<b;}
+
