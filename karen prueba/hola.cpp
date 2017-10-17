01b2444d4b108ce586efd22846e6e6d6b1a49a03
Karen Denegri <31105002+Karendc1d@users.noreply.github.com>
2017-10-17 09:26:49 -0500

    Update hola.cpp

--- karen prueba/hola.cpp
+++ karen prueba/hola.cpp
@@ -6,80 +6,80 @@ using namespace std;
 
 int main(){
 
-  Equipo x=("Bayern Munchen!, Jugadores(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11));
+  Equipo x=("Bayern Munchen", class Jugadores(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11));
     x.competir();
 
-  Jugadores p1=("Neuer";"arquero";1;3);
-    p1.jugar();
-    p1.print();
-  Jugadores p2=("Gotze";"defensa";20;6);
-    p2.jugar();
-    p2.print();
-   Jugadores p3=("Hummels";"defensa";5;5);
-    p3.jugar();
-    p3.print();
-   Jugadores p4=("Rafinha";"defensa";13;6);
-    p4.jugar();
-    p4.print();
-   Jugadores p5=("Ribéry";"centrocampista";7;3);
-    p5.jugar();
-    p5.print();
-   Jugadores p6=("Xabi";"centrocampista";14;6);
-    p6.jugar();
-    p6.print();
-   Jugadores p7=("Kimmich";"centrocampista";32;8);
-    p7.jugar();
-    p7.print();
-   Jugadores p8=("Lewandowski";"delantero";9;9);
-    p8.jugar();
-    p8.print();
-   Jugadores p9=("Müller";"delantero";25;8);
-    p9.jugar();
-    p9.print();
-   Jugadores p10=("Robben";"delantero";10;7);
-    p10.jugar();
-    p10.print();
-   Jugadores p11=("Lahm";"ccentrocampista";21;6);
-    p11.jugar();
-    p11.print();
+          Jugadores p1=("Neuer";"arquero";1;3);
+            p1.jugar();
+            p1.print();
+          Jugadores p2=("Gotze";"defensa";20;6);
+            p2.jugar();
+            p2.print();
+           Jugadores p3=("Hummels";"defensa";5;5);
+            p3.jugar();
+            p3.print();
+           Jugadores p4=("Rafinha";"defensa";13;6);
+            p4.jugar();
+            p4.print();
+           Jugadores p5=("Ribéry";"centrocampista";7;3);
+            p5.jugar();
+            p5.print();
+           Jugadores p6=("Xabi";"centrocampista";14;6);
+            p6.jugar();
+            p6.print();
+           Jugadores p7=("Kimmich";"centrocampista";32;8);
+            p7.jugar();
+            p7.print();
+           Jugadores p8=("Lewandowski";"delantero";9;9);
+            p8.jugar();
+            p8.print();
+           Jugadores p9=("Müller";"delantero";25;8);
+            p9.jugar();
+            p9.print();
+           Jugadores p10=("Robben";"delantero";10;7);
+            p10.jugar();
+            p10.print();
+           Jugadores p11=("Lahm";"ccentrocampista";21;6);
+            p11.jugar();
+            p11.print();
 
  Equipo y=("Borrusia Dortmun", class Jugadores(q1,q2,q3,q4,q6,q7,q8,q9,q10,q11));
 
     y.competir();
 
-   Jugadores q1=("Reus", "Delantero", 11, 8);
-    q1.jugar();
-    q1.print();
-  Jugadores q2=("Abameyang", "Delantero centro", 17, 8);
-    q2.jugar();
-    q2.print();
-   Jugadores q3=("Dembele", "Medio Campista", 11, 9);
-    q3.jugar();
-    q3.print();
-   Jugadores q4=("Kagawa", "Medio Campista", 23, 7);
-    q4.jugar();
-    q4.print();
-   Jugadores q5=("Burki", "Portero", 1, 5);
-    q5.jugar();
-    q5.print();
-   Jugadores q6=("  Sokratis", "Defensa", 25, 5);
-    q6.jugar();
-    q6.print();
-   Jugadores q7=("Bartra", "Defensa", 5, 6);
-    q7.jugar();
-    q7.print();
-   Jugadores q8=("Subotic", "Defensa", 4, 4);
-    q8.jugar();
-    q8.print();
-   Jugadores q9=("Gotze", "Delantero", 10, 9);
-    q9.jugar();
-    q9.print();
-   Jugadores q10=("Castro", "Defensa", 27, 4);
-    q10.jugar();
-    q10.print();
-   Jugadores q11=("Rode", "Volante", 18, 5);
-    q11.jugar();
-    q11.print();
+           Jugadores q1=("Reus", "Delantero", 11, 8);
+            q1.jugar();
+            q1.print();
+          Jugadores q2=("Abameyang", "Delantero centro", 17, 8);
+            q2.jugar();
+            q2.print();
+           Jugadores q3=("Dembele", "Medio Campista", 11, 9);
+            q3.jugar();
+            q3.print();
+           Jugadores q4=("Kagawa", "Medio Campista", 23, 7);
+            q4.jugar();
+            q4.print();
+           Jugadores q5=("Burki", "Portero", 1, 5);
+            q5.jugar();
+            q5.print();
+           Jugadores q6=("  Sokratis", "Defensa", 25, 5);
+            q6.jugar();
+            q6.print();
+           Jugadores q7=("Bartra", "Defensa", 5, 6);
+            q7.jugar();
+            q7.print();
+           Jugadores q8=("Subotic", "Defensa", 4, 4);
+            q8.jugar();
+            q8.print();
+           Jugadores q9=("Gotze", "Delantero", 10, 9);
+            q9.jugar();
+            q9.print();
+           Jugadores q10=("Castro", "Defensa", 27, 4);
+            q10.jugar();
+            q10.print();
+           Jugadores q11=("Rode", "Volante", 18, 5);
+            q11.jugar();
+            q11.print();
 
   return 0;
 }
@@ -115,6 +115,7 @@ void competir(){
   cout<<"¡El equipo "<<nombre<<" competirá hoy!"<<endl;
 }
 };
+
 
 class Jugadores{
   private:
